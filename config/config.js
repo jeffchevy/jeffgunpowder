c888dd46f505a0e2fa21de87b59bad4a2ffc335f
Jeremy Stowell <jeremy.stowell@gmail.com>
2016-04-09 12:11:00 -0600

    Changed the database to use our shared account on mlab.

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'port': process.env.PORT || '1337',
-    'database': process.env.DATABASE || 'mongodb://drillMaster:sandboxMaster@ds027835.mongolab.com:27835/gunpowder_sandbox',
+    'database': process.env.DATABASE || 'mongodb://gunpowder-dev:1j7d98ko00W@ds021000.mlab.com:21000/gunpowder-dev',
     'secret': 'djufikdjshuyejkopwqmnjuidusijd',
     'tokenExpiration': process.env.TOKEN || 1440 //24 hours
 };
