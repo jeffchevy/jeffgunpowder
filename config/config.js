fe177e6d399a32ed4e9d6dbc2e25365e68ff00aa
Jeremy Stowell <jeremy.stowell@gmail.com>
2016-05-09 21:16:49 -0600

    Fixed #35 - Changed the default from 24 hours to 1 week.

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     'port': process.env.PORT || '1337',
     'database': process.env.DATABASE || 'mongodb://gunpowder-dev:1j7d98ko00W@ds021000.mlab.com:21000/gunpowder-dev',
     'secret': process.env.SECRET || 'djufikdjshuyejkopwqmnjuidusijd',
-    'tokenExpiration': process.env.TOKEN || 1440 //24 hours
+    'tokenExpiration': process.env.TOKEN || 10080 // 1 week  -- Value in # of minutes
 };
